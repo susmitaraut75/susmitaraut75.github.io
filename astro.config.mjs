1f26051dc3dfc402bc2c306ad08cbe32521bcf3d
Sajan Basnet <sajan@fetch.ly>
2026-06-04 15:08:33 +1000

    Initial commit: Susmita Raut Portfolio Website

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://susmitaraut75.github.io',
+  // base: '/repo-name', // Uncomment and set if deploying to username.github.io/repo-name
+  output: 'static',
+});
